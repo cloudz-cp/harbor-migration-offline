edde75f044408a8565feaf4a4570c73e6b38a9f3
azssi <voidmain0805@gmail.com>
2023-01-19 10:04:50 +0900

    fix to include missing images over 100th

--- container-images/export-imgs.sh
+++ container-images/export-imgs.sh
@@ -9,15 +9,27 @@ function loginHarbor() {
   echo $PASSWORD | skopeo login $DOMAIN -u $USER --password-stdin
 }
 
-function listRepo() {
-  cmd=$(curl -s "https://$DOMAIN/api/v2.0/projects/$1/repositories?page=1&page_size=100" \
+function countRepo() {
+  proj=$1
+  cmd=$(curl -s "https://$DOMAIN/api/v2.0/projects/$proj/summary" \
     -H "accept: application/json" \
     -u $USER:$PASSWORD)
-  echo $cmd | jq -c '.[] | .name' | sed 's/"//g' | sed "s/^$1\///"
+  echo $cmd | jq -c '.repo_count' | sed 's/"//g'
+}
+
+function listRepo() {
+  proj=$1
+  page=$2
+  cmd=$(curl -s "https://$DOMAIN/api/v2.0/projects/$proj/repositories?page=$page&page_size=100" \
+    -H "accept: application/json" \
+    -u $USER:$PASSWORD)
+  echo $cmd | jq -c '.[] | .name' | sed 's/"//g' | sed "s/^$proj\///"
 }
 
 function getTags() {
-  cmd=$(curl -s "https://$DOMAIN/api/v2.0/projects/$1/repositories/$2/artifacts" \
+  proj=$1
+  repo=$2
+  cmd=$(curl -s "https://$DOMAIN/api/v2.0/projects/$proj/repositories/$repo/artifacts" \
     -H "accept: application/json" \
     -u $USER:$PASSWORD)
   echo $cmd | jq '.[] | if has("tags") then select(.tags) | .tags | .[] | .name else "latest" end' | sed 's/"//g'
@@ -30,11 +42,19 @@ function downloadTaggedRepo() {
   then
     mkdir -p $proj/download
   fi
+
+  if [ ! -e $proj/repositories.txt ]
+  then
+    pages=$((($(countRepo cloudzcp-public)+99)/100))
+    for i in $(seq 1 $pages)
+    do
+      listRepo $proj $i >> $proj/repositories.txt
+    done
+  fi
+
   if [ ! -e $proj/targets.txt ]
   then
-    cat /dev/null > $proj/targets.txt
-    repos=($(listRepo $proj))
-    for repo in $repos
+    while read repo
     do
       encodedName=$(echo $repo | sed "s/\//%2F/g")
       tags=($(getTags $proj $encodedName))
@@ -42,7 +62,7 @@ function downloadTaggedRepo() {
       do
         echo "$repo:$tag" >> $proj/targets.txt
       done
-    done
+    done < $proj/repositories.txt
   fi
 
   while read line
@@ -60,5 +80,5 @@ function downloadTaggedRepo() {
 
 loginHarbor
 downloadTaggedRepo "cloudzcp"
-downloadTaggedRepo "cloudzcp-public"
 downloadTaggedRepo "cloudzcp-addon"
+downloadTaggedRepo "cloudzcp-public"
